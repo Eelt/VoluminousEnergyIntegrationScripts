cf3c5eaa3187b2e3e20b54e3b711fb8920501b71
Eelt <davidbuddy9@gmail.com>
2021-07-04 16:35:40 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# What is this all about?
+Other mods may add new materials or items that would logically make sence to be processed in Voluminous Energy machines, but because Voluminous Energy doesn't have the recipes baked in, those machines can't process those items.
+
+The solution to this problem, in all mods, has always been to use some sort of 3rd party tool, such as KubeJS or CraftTweaker to modify crafting recipes or add new ones to ensure proper functionality, especially in a modpack environment. Especially since using Forge and vanilla tags can only go so far.
+
+Scripts that end with .js are JavaScript scripts intended for use with KubeJS.
+
+# What does this add?
+So far:
+* All The Mods 6 (all the mod ores for the crusher)
---
+++ voluminousenergy_atm6.js
@@ -0,0 +1,437 @@
+events.listen('recipes', e => {
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_aluminum",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:aluminum_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_aluminum_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_aluminum",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:aluminum_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_aluminum_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_copper",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:copper_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_copper_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_copper",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:copper_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_copper_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_lead",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:lead_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "voluminousenergy:galena_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_lead",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:lead_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "voluminousenergy:galena_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_nickel",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:nickel_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_nickel_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_nickel",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:nickel_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_nickel_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_osmium",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:osmium_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_osmium_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_osmium",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:osmium_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_osmium_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_platinum",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:platinum_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_platinum_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_platinum",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:platinum_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_platinum_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_silver",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:silver_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "voluminousenergy:galena_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_silver",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:silver_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "voluminousenergy:galena_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_tin",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:tin_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_tin_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_tin",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:tin_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_tin_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_uranium",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:uranium_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_uranium_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_uranium",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:uranium_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_uranium_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_zinc",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:zinc_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_zinc_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_zinc",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:zinc_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "alltheores:dirty_zinc_dust",
+          "count": 1,
+          "chance": 1
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_iron",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "alltheores:iron_dust",
+          "count": 2
+        },
+        "rng": {
+          "item": "minecraft:air",
+          "count": 0,
+          "chance": 0
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_coal",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "minecraft:coal",
+          "count": 3
+        },
+        "rng": {
+          "item": "minecraft:air",
+          "count": 0,
+          "chance": 0
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_redstone",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "minecraft:redstone",
+          "count": 6
+        },
+        "rng": {
+          "item": "minecraft:air",
+          "count": 0,
+          "chance": 0
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_diamond",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "minecraft:diamond",
+          "count": 2
+        },
+        "rng": {
+          "item": "minecraft:air",
+          "count": 0,
+          "chance": 0
+        }
+    }),
+    e.custom({
+        "type": "voluminousenergy:crushing",
+        "ingredient": {
+          "item": "alltheores:ore_other_lapis",
+          "count": 1
+        },
+        "process_time": 200,
+        "result": {
+          "item": "minecraft:lapis_lazuli",
+          "count": 6
+        },
+        "rng": {
+          "item": "minecraft:air",
+          "count": 0,
+          "chance": 0
+        }
+    }),
+    e.remove({output: 'voluminousenergy:silicon'}),
+    e.custom({
+        "type": "minecraft:smelting",
+        "ingredient": {
+          "tag": "forge:dusts/sand"
+        },
+        "result": "refinedstorage:silicon",
+        "experience": 0.1,
+        "cookingtime": 200
+    })
+})
